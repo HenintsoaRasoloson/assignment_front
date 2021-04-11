8ec0d8ceccf2dfa8018f6edb3c6e2aad1b5114da
HenintsoaRasoloson <henintsoa.rasoloson@gmail.com>
2021-04-11 15:03:19 +0300

    add tabs module into apps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatTabsModule} from '@angular/material/tabs';
 
 
 import { AssignmentsComponent } from './assignments/assignments.component';
@@ -73,6 +74,7 @@ const routes:Routes = [
     MatFormFieldModule, MatInputModule, MatDatepickerModule,
     MatNativeDateModule, MatListModule, MatCardModule, MatCheckboxModule,
     MatSlideToggleModule,
+    MatTabsModule,
     RouterModule.forRoot(routes), HttpClientModule
   ],
   providers: [],
